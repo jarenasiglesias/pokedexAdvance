95d8aff6555c71546cff6ea602b5331d94b5236c
Javier Arenas Iglesias <javierareigl@gmail.com>
2017-07-20 19:44:06 +0200

    added images in the pokedex

--- js/pokemon.js
+++ js/pokemon.js
@@ -2,26 +2,29 @@ var $textBoxPokemon = $('#searchPokemon');
 var $textBoxType = $('#searchType');
 var $buttonPokemon = $('#btn-pokemon');
 var $buttonType = $('#btn-type');
-var obj;
 
 $buttonPokemon.on("click", function(e) {
   e.preventDefault();
   $.get('http://pokeapi.co/api/v1/pokemon/' + $textBoxPokemon.val()+"/", function getPokemon (response){  
- 
   var types = response.types;
   var descriptionUri = response.descriptions[1].resource_uri;
+  var imgUri = "http://pokeapi.co" + response.sprites[0].resource_uri;
   var urlApi = "http://pokeapi.co" + descriptionUri;
   $('.row-bg').removeAttr("hidden");
   $('.empty').children().empty();
   $('.pkmnName').append("<h2>" + "Name: " + response.name + "</h2>");
   $('h3').append("Type: ");
   response.types.forEach(function(element,i,types){  
-  $('h3').append(types[i].name +" ");
-   })
+    $('h3').append(types[i].name +" ");
+    })
+  $.get(imgUri, function getDescription( response){
+    $('.pkmnImg').attr('src',"http://pokeapi.co" + response.image);
+    });
+
   $('.descTitle').append("<h2>" + "Description: " + "</h2>");
+
   $.get(urlApi, function getDescription( response){
     $('.desc').append(response.description);
-
     });
   
   });
@@ -33,8 +36,7 @@ $buttonType.on("click", function(e) {
   $.get('http://pokeapi.co/api/v2/type/' + $textBoxType.val()+"/", function getType (response){  
   $('.empty').children().empty();
   var pokemonsNames = [];
-  var pokemons = response.pokemon
-  console.log(response);
+  var pokemons = response.pokemon;
   $('.row-bg').removeAttr("hidden");
   $('.pkmnName').append("<h2>" + "Name:" + "</h2>");
   $('h3').append("Type: " + response.name);
